61481e50f66254aca09ab694506d3cf619211baf
ABHISHEK BHARDWAJ <59165472+abhishekbhardwaj2023@users.noreply.github.com>
2022-01-12 23:46:59 +0530

    Update money_machine.py

--- money_machine.py
+++ money_machine.py
@@ -27,6 +27,7 @@ class MoneyMachine:
     def make_payment(self, cost):
         """Returns True when payment is accepted, or False if insufficient."""
         self.process_coins()
+        
         if self.money_received >= cost:
             change = round(self.money_received - cost, 2)
             print(f"Here is {self.CURRENCY}{change} in change.")
@@ -37,4 +38,3 @@ class MoneyMachine:
             print("Sorry that's not enough money. Money refunded.")
             self.money_received = 0
             return False
-        
